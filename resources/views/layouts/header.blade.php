e760bb5eeb1480a28a6b98d88448f2eea5a49903
maryam-amn <Maryam.Aman@jobtrek.ch>
2025-06-16 15:53:42 +0200

    feat: Creation of the page where there is all the avaible apartements

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,5 @@
+<header class="flex gap-10 p-4 bg-violet-50  justify-center ">
+    <a href="http://localhost/Register" ><p> Register</p></a>
+    <a href="http://localhost/Login" ><p> Log in </p></a>
+    <a href="http://localhost/Login" ><p> Homepage </p></a>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
-
+*/
 Route::get('/Login', function () {
     return view('login');
 });
 Route::get('/Register', function () {
     return view('register');
+});
+Route::get('/', function () {
+    return view('apprtementAvaible');
 });
